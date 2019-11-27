f87c4dc8fbee17c46f267775e340c1b764fe970b
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-11-27 11:59:43 +0530

    final

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', (req, res) => {
+  console.log('Client Post', req.body);
+  res.send({ message: 'Received' });
+});
+
+router.put('/', (req, res) => {
+  console.log('Client Put', req.body);
+  res.send({ message: 'Received' });
+});
+
+router.get('/validate/:key', function(req, res, next) {
+  if (req.params.key === 'ABCD') {
+    res.send({ valid: true });
+  } else {
+    res.send({ valid: false });
+  }
+});
+
 module.exports = router;
